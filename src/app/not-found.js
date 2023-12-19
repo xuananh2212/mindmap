b979b3eed0d3ce49a05760fc89a1c618c6e7bb1a
xuananh2212 <xuantuananh2212@gmail.com>
2023-12-19 15:51:26 +0700

    add page not-found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,13 +1,23 @@
 import Link from "next/link"
+import Image from "next/image";
+import img404 from '../../public/imgs/404.svg'
 export default function NotFound() {
      return (
-          <div>
-               <h1>Page Not Found</h1>
-               <p>Lạc đường rồi . vui lòng quay về trang chủ </p>
-               <Link
-                    href="/">
-                    Về Trang Chủ
-               </Link>
+          <div className="h-screen w-screen bg-gray-50 flex items-center">
+               <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
+                    <div className="w-full lg:w-1/2 mx-8">
+                         <div className="text-7xl text-green-500 font-dark font-extrabold mb-8"> 404</div>
+                         <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
+                              Sorry we couldn't find the page you're looking for
+                         </p>
+
+                         <a href="#" className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700">back to homepage</a>
+                    </div>
+                    <div className="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
+                         <Image width={500} height={600} src={img404.src} className="" alt="Page not found" />
+                    </div>
+
+               </div>
           </div>
      )
 }
