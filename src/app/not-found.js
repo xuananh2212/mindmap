ca3aae5d1bafc1e33d071cc0cb48f8a578261c3e
xuananh2212 <xuantuananh2212@gmail.com>
2023-12-19 16:28:31 +0700

    fix bug page 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -8,7 +8,7 @@ export default function NotFound() {
                     <div className="w-full lg:w-1/2 mx-8">
                          <div className="text-7xl text-green-500 font-dark font-extrabold mb-8"> 404</div>
                          <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
-                              Sorry we couldn't find the page you're looking for
+                              Sorry we could not find the page you are looking for
                          </p>
 
                          <a href="#" className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700">back to homepage</a>
