7ddbae211074f3334828bca5af22828aa4c5da2c
xuananh2212 <xuantuananh2212@gmail.com>
2023-12-24 14:58:21 +0700

    fix bug share private

--- src/app/(home)/mindmap/[id]/page.js
+++ src/app/(home)/mindmap/[id]/page.js
@@ -33,7 +33,7 @@ export default async function MindMapDetail({ params }) {
                redirect("/api/auth/login");
           } else {
                if (data.idUser !== idUser) {
-                    redirect("/api/auth/login");
+                    notFound();
                }
           }
      }
